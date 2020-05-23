e9f0294cd7c51fcb3e19a9eb9be3c2f104dd4802
cullenk <45953462+cullenk@users.noreply.github.com>
2020-05-23 16:56:04 -0400

    Final additions

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
-const BLACK_KEYS = ['w', 'e', 't', 'y', 'u', 'o']
+const loadingScreen = document.getElementById('loading-screen');
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '=']
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u', 'o', 'p']
 
-const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
+const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+const spacebarSound = document.getElementById('spacebar');
+const spacebar = document.querySelector('.spacebar');
+
+window.addEventListener('load', function () { //Wait until the page loads...
+  loadingScreen.className += " loaderHidden"; //then remove the loading
+});
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
 })
+
 
 document.addEventListener('keydown', e => {
   if (e.repeat) return
@@ -20,11 +28,37 @@ document.addEventListener('keydown', e => {
 })
 
 function playNote(key) {
-  const noteAudio = document.getElementById(key.dataset.note)
-  noteAudio.currentTime = 0
-  noteAudio.play()
-  key.classList.add('active')
+  const noteAudio = document.getElementById(key.dataset.note);
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add('active');
   noteAudio.addEventListener('ended', () => {
-    key.classList.remove('active')
+    key.classList.remove('active');
   })
 }
+
+// Spacebar functionality 
+
+spacebarSound.addEventListener('click', () => {
+  playVoldy();
+  spacebar.classList.add('active');
+  spacebarSound.addEventListener('ended', () => {
+    spacebar.classList.remove('active');
+    })
+})
+document.addEventListener('keydown', e => {
+  if (e.code === 'Space') {
+    if (e.repeat) return;
+    playVoldy(e);
+  }
+})
+
+function playVoldy(){
+  spacebarSound.play();
+    spacebarSound.currentTime = 0; 
+    spacebarSound.play();
+    spacebar.classList.add('active');
+    spacebarSound.addEventListener('ended', () => {
+      spacebar.classList.remove('active');
+      })
+    }
